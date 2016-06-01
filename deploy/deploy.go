84b4f5d1790369dd1e2669014a9c05e0e0888dca
Andrew Slotin <andrew.slotin@gmail.com>
2016-06-01 21:51:05 +0200

    Add deploy type

---
+++ deploy/deploy.go
@@ -0,0 +1,13 @@
+package deploy
+
+import (
+	"time"
+
+	"github.com/andrewslotin/slack-deploy-command/slack"
+)
+
+type Deploy struct {
+	User      slack.User
+	Subject   string
+	StartedAt time.Time
+}
