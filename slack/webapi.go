c9a55ba03443e38369c87076a782c7051f3ed462
Andrew Slotin <andrew.slotin@gmail.com>
2016-06-16 23:52:15 +0200

    Add Slack WebAPI client implementing channels.setTopic

---
+++ slack/webapi.go
@@ -0,0 +1,103 @@
+package slack
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const SlackWebAPIEndpoint = "https://slack.com/api"
+
+type WebAPI struct {
+	c     *http.Client
+	token string
+
+	BaseURL string
+}
+
+func NewWebAPI(token string, httpClient *http.Client) *WebAPI {
+	api := &WebAPI{
+		token:   token,
+		c:       httpClient,
+		BaseURL: SlackWebAPIEndpoint,
+	}
+
+	if api.c == nil {
+		api.c = http.DefaultClient
+	}
+
+	return api
+}
+
+func (api *WebAPI) SetChannelTopic(channelID, topic string) error {
+	const method = "channels.setTopic"
+
+	req, err := api.newRequestWithToken(method)
+	if err != nil {
+		return err
+	}
+
+	q := req.URL.Query()
+	q.Add("channel_id", channelID)
+	q.Add("topic", topic)
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := api.c.Do(req)
+	if err != nil {
+		return wrapError(fmt.Errorf("failed to call method (%s)", err), method, req.URL)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return wrapError(fmt.Errorf("failed to read response body (%s)", err), method, req.URL)
+	}
+
+	if resp.StatusCode >= http.StatusBadRequest {
+		return wrapError(fmt.Errorf("WebAPI responded with HTTP %d %q", resp.StatusCode, body), method, req.URL)
+	}
+
+	var v struct {
+		Ok    bool   `json:"ok"`
+		Error string `json:"error"`
+	}
+	if err := json.Unmarshal(body, &v); err != nil {
+		return wrapError(fmt.Errorf("failed to decode response body %q (%s)", body, err), method, req.URL)
+	}
+
+	if !v.Ok {
+		return wrapError(fmt.Errorf("WebAPI returned error (%s)", v.Error), method, req.URL)
+	}
+
+	return nil
+}
+
+func (api *WebAPI) newRequestWithToken(method string) (*http.Request, error) {
+	req, err := http.NewRequest("GET", api.BaseURL+"/"+method, nil)
+	if err != nil {
+		return nil, wrapError(fmt.Errorf("failed to build WebAPI request (%s)", err), method, nil)
+	}
+
+	q := req.URL.Query()
+	q.Add("token", api.token)
+	req.URL.RawQuery = q.Encode()
+
+	return req, nil
+}
+
+func wrapError(err error, method string, url *url.URL) *WebAPIError {
+	e := &WebAPIError{
+		Method:   method,
+		URL:      url.String(),
+		Response: err.Error(),
+	}
+
+	if url != nil {
+		url.Query().Set("token", "[hidden]")
+		e.URL = url.String()
+	}
+
+	return e
+}
---
+++ slack/webapi_test.go
@@ -0,0 +1,93 @@
+package slack_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/andrewslotin/slack-deploy-command/slack"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWebAPI_ChannelsSetTopic(t *testing.T) {
+	mux, baseURL, teardown := setup()
+	defer teardown()
+
+	var requestNum int
+	mux.HandleFunc("/channels.setTopic", func(w http.ResponseWriter, r *http.Request) {
+		requestNum++
+
+		assert.Equal(t, "CHANNELID1", r.FormValue("channel_id"))
+		assert.Equal(t, "xxxx-token-12345", r.FormValue("token"))
+		assert.Equal(t, "Example topic", r.FormValue("topic"))
+
+		w.Write([]byte(`{"ok":true,"topic":"Example topic"}`))
+	})
+
+	api := slack.NewWebAPI("xxxx-token-12345", nil)
+	api.BaseURL = baseURL
+
+	require.NoError(t, api.SetChannelTopic("CHANNELID1", "Example topic"))
+	require.Equal(t, 1, requestNum)
+}
+
+func TestWebAPI_ChannelsSetTopic_ErrorHandling(t *testing.T) {
+	mux, baseURL, teardown := setup()
+	defer teardown()
+
+	var requestNum int
+	mux.HandleFunc("/channels.setTopic", func(w http.ResponseWriter, r *http.Request) {
+		requestNum++
+
+		assert.Equal(t, "CHANNELID1", r.FormValue("channel_id"))
+		assert.Equal(t, "xxxx-token-12345", r.FormValue("token"))
+		assert.Equal(t, "Example topic", r.FormValue("topic"))
+
+		w.Write([]byte(`{"ok":false,"error":"channel not found"}`))
+	})
+
+	api := slack.NewWebAPI("xxxx-token-12345", nil)
+	api.BaseURL = baseURL
+
+	err := api.SetChannelTopic("CHANNELID1", "Example topic")
+	require.Equal(t, 1, requestNum)
+
+	if assert.EqualError(t, err, "WebAPI returned error (channel not found)") {
+		var slackErr *slack.WebAPIError
+		if assert.IsType(t, slackErr, err) {
+			slackErr = err.(*slack.WebAPIError)
+			assert.Equal(t, "channels.setTopic", slackErr.Method)
+			assert.NotEmpty(t, slackErr.URL)
+		}
+	}
+}
+
+func TestWebAPI_ChannelsSetTopic_HTTPFailure(t *testing.T) {
+	mux, baseURL, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/channels.setTopic", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "An error occurred", http.StatusInternalServerError)
+	})
+
+	api := slack.NewWebAPI("xxxx-token-12345", nil)
+	api.BaseURL = baseURL
+
+	err := api.SetChannelTopic("CHANNELID1", "Example topic")
+	if assert.EqualError(t, err, `WebAPI responded with HTTP 500 "An error occurred\n"`) {
+		var slackErr *slack.WebAPIError
+		if assert.IsType(t, slackErr, err) {
+			slackErr = err.(*slack.WebAPIError)
+			assert.Equal(t, "channels.setTopic", slackErr.Method)
+			assert.NotEmpty(t, slackErr.URL)
+		}
+	}
+}
+
+func setup() (mux *http.ServeMux, baseURL string, teardownFn func()) {
+	mux = http.NewServeMux()
+	ts := httptest.NewServer(mux)
+
+	return mux, ts.URL, ts.Close
+}
