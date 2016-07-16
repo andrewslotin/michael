acf5595cfb4d97ed485717036729a5b40b387f88
Andrew Slotin <andrew.slotin@gmail.com>
2016-07-16 13:55:19 +0200

    Print build info with --version

--- main.go
+++ main.go
@@ -27,22 +27,35 @@ var (
 	builder        = "n/a"
 
 	args struct {
-		host string
-		port int
+		host         string
+		port         int
+		printVersion bool
 	}
 )
 
 func init() {
+	flag.BoolVar(&args.printVersion, "version", false, "Print version and exit")
 	flag.StringVar(&args.host, "h", DefaultHost, "Host or address to listen on")
 	flag.IntVar(&args.port, "p", DefaultPort, "Port to listen on")
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n\nOptions:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n\nOptions:\n", binPath)
 		flag.PrintDefaults()
 	}
 }
 
+func printVersion() {
+	fmt.Printf("%s v%s (rev %s)\n", binPath, version, buildRev)
+	fmt.Printf("Built with %s on %s by %s\n\n", buildGoVersion, buildDate, builder)
+	fmt.Println("Found a bug? Got an idea? Open an issue on https://github.com/andrewslotin/slack-deploy-command\nContributions are welcome!")
+	os.Exit(0)
+}
+
 func main() {
 	flag.Parse()
+
+	if args.printVersion {
+		printVersion()
+	}
 
 	slackToken := os.Getenv("SLACK_TOKEN")
 	if slackToken == "" {
