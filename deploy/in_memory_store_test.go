10a9640829b0e6d5fb2f8adb65e1228400deda10
Andrew Slotin <andrew.slotin@gmail.com>
2016-07-26 01:33:32 +0200

    Use StoreSuite to test InMemoryStore

--- deploy/in_memory_store_test.go
+++ deploy/in_memory_store_test.go
@@ -2,83 +2,13 @@ package deploy_test
 
 import (
 	"testing"
-	"time"
 
 	"github.com/andrewslotin/slack-deploy-command/deploy"
-	"github.com/andrewslotin/slack-deploy-command/slack"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestInMemoryStore_GetSet(t *testing.T) {
-	store := deploy.NewInMemoryStore()
-
-	_, ok := store.Get("key1")
-	assert.False(t, ok)
-
-	// Store a value
-	store.Set("key1", deploy.New(slack.User{ID: "1", Name: "Test User"}, "Deploy subject"))
-	if d, ok := store.Get("key1"); assert.True(t, ok) {
-		assert.Equal(t, "1", d.User.ID)
-		assert.Equal(t, "Test User", d.User.Name)
-		assert.Equal(t, "Deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-
-	// Override existing value
-	store.Set("key1", deploy.New(slack.User{ID: "2", Name: "First User"}, "Updated deploy subject"))
-	if d, ok := store.Get("key1"); assert.True(t, ok) {
-		assert.Equal(t, "2", d.User.ID)
-		assert.Equal(t, "First User", d.User.Name)
-		assert.Equal(t, "Updated deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-
-	// Populate another key
-	store.Set("key2", deploy.New(slack.User{ID: "3", Name: "Second User"}, "Another deploy"))
-	if d, ok := store.Get("key2"); assert.True(t, ok) {
-		assert.Equal(t, "3", d.User.ID)
-		assert.Equal(t, "Second User", d.User.Name)
-		assert.Equal(t, "Another deploy", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-
-	// Check that another record wasn't changed
-	if d, ok := store.Get("key1"); assert.True(t, ok) {
-		assert.Equal(t, "2", d.User.ID)
-		assert.Equal(t, "First User", d.User.Name)
-		assert.Equal(t, "Updated deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-}
-
-func TestInMemoryStore_Del(t *testing.T) {
-	store := deploy.NewInMemoryStore()
-
-	_, ok := store.Del("key1")
-	assert.False(t, ok)
-
-	store.Set("key1", deploy.New(slack.User{ID: "1", Name: "First User"}, "Deploy subject"))
-	store.Set("key2", deploy.New(slack.User{ID: "2", Name: "Second User"}, "Another deploy"))
-
-	_, ok = store.Get("key1")
-	require.True(t, ok)
-	_, ok = store.Get("key2")
-	require.True(t, ok)
-
-	if d, ok := store.Del("key1"); assert.True(t, ok) {
-		assert.Equal(t, "1", d.User.ID)
-		assert.Equal(t, "First User", d.User.Name)
-		assert.Equal(t, "Deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-	}
-
-	_, ok = store.Get("key1")
-	assert.False(t, ok)
-	_, ok = store.Get("key2")
-	assert.True(t, ok)
+func TestInMemoryStore(t *testing.T) {
+	suite.Run(t, &StoreSuite{Setup: func() (store deploy.Store, teardownFn func(), err error) {
+		return deploy.NewInMemoryStore(), nil, nil
+	}})
 }
