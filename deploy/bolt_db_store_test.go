a7dec91ec66c43f207fed006abc841739b5fda93
Andrew Slotin <andrew.slotin@gmail.com>
2016-07-26 01:33:44 +0200

    Use StoreSuite to test BoltDBStore

--- deploy/bolt_db_store_test.go
+++ deploy/bolt_db_store_test.go
@@ -4,99 +4,24 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/andrewslotin/slack-deploy-command/deploy"
-	"github.com/andrewslotin/slack-deploy-command/slack"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestBoltDBStore_GetSet(t *testing.T) {
-	path, teardown := setup(t)
-	defer teardown()
+func TestBoltDBStore(t *testing.T) {
+	suite.Run(t, &StoreSuite{Setup: func() (store deploy.Store, teardownFn func(), err error) {
+		fd, err := ioutil.TempFile(os.TempDir(), "doppelganger")
+		if err != nil {
+			return nil, nil, err
+		}
+		fd.Close()
 
-	store, err := deploy.NewBoltDBStore(path)
-	require.NoError(t, err)
+		store, err = deploy.NewBoltDBStore(fd.Name())
+		if err != nil {
+			return nil, func() { os.Remove(fd.Name()) }, err
+		}
 
-	_, ok := store.Get("key1")
-	assert.False(t, ok)
-
-	// Store a value
-	store.Set("key1", deploy.New(slack.User{ID: "1", Name: "Test User"}, "Deploy subject"))
-	if d, ok := store.Get("key1"); assert.True(t, ok) {
-		assert.Equal(t, "1", d.User.ID)
-		assert.Equal(t, "Test User", d.User.Name)
-		assert.Equal(t, "Deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-
-	// Override existing value
-	store.Set("key1", deploy.New(slack.User{ID: "2", Name: "First User"}, "Updated deploy subject"))
-	if d, ok := store.Get("key1"); assert.True(t, ok) {
-		assert.Equal(t, "2", d.User.ID)
-		assert.Equal(t, "First User", d.User.Name)
-		assert.Equal(t, "Updated deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-
-	// Populate another key
-	store.Set("key2", deploy.New(slack.User{ID: "3", Name: "Second User"}, "Another deploy"))
-	if d, ok := store.Get("key2"); assert.True(t, ok) {
-		assert.Equal(t, "3", d.User.ID)
-		assert.Equal(t, "Second User", d.User.Name)
-		assert.Equal(t, "Another deploy", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-
-	// Check that another record wasn't changed
-	if d, ok := store.Get("key1"); assert.True(t, ok) {
-		assert.Equal(t, "2", d.User.ID)
-		assert.Equal(t, "First User", d.User.Name)
-		assert.Equal(t, "Updated deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-		assert.True(t, d.FinishedAt.IsZero())
-	}
-}
-
-func TestBoltDBStore_Del(t *testing.T) {
-	path, teardown := setup(t)
-	defer teardown()
-
-	store, err := deploy.NewBoltDBStore(path)
-	require.NoError(t, err)
-
-	_, ok := store.Del("key1")
-	assert.False(t, ok)
-
-	store.Set("key1", deploy.New(slack.User{ID: "1", Name: "First User"}, "Deploy subject"))
-	store.Set("key2", deploy.New(slack.User{ID: "2", Name: "Second User"}, "Another deploy"))
-
-	_, ok = store.Get("key1")
-	require.True(t, ok)
-	_, ok = store.Get("key2")
-	require.True(t, ok)
-
-	if d, ok := store.Del("key1"); assert.True(t, ok) {
-		assert.Equal(t, "1", d.User.ID)
-		assert.Equal(t, "First User", d.User.Name)
-		assert.Equal(t, "Deploy subject", d.Subject)
-		assert.WithinDuration(t, time.Now(), d.StartedAt, time.Second)
-	}
-
-	_, ok = store.Get("key1")
-	assert.False(t, ok)
-	_, ok = store.Get("key2")
-	assert.True(t, ok)
-}
-
-func setup(t *testing.T) (path string, teardownFn func()) {
-	fd, err := ioutil.TempFile(os.TempDir(), "doppelganger")
-	require.NoError(t, err)
-	fd.Close()
-
-	return fd.Name(), func() { os.Remove(fd.Name()) }
+		return store, func() { os.Remove(fd.Name()) }, nil
+	}})
 }
