1b59df0e262bc76f12acfc147c56b42f526dd5cd
Andrew Slotin <andrew.slotin@gmail.com>
2016-08-04 23:25:08 +0200

    Add deploy.Repository

--- deploy/bolt_db_store_test.go
+++ deploy/bolt_db_store_test.go
@@ -1,68 +1,56 @@
 package deploy_test
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/andrewslotin/slack-deploy-command/deploy"
-	"github.com/andrewslotin/slack-deploy-command/slack"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
-func TestBoltDBStore(t *testing.T) {
+func TestBoltDBStore_AsStore(t *testing.T) {
 	suite.Run(t, &StoreSuite{Setup: func() (store deploy.Store, teardownFn func(), err error) {
-		fd, err := ioutil.TempFile(os.TempDir(), "doppelganger")
+		path, err := tempDBFilePath()
 		if err != nil {
 			return nil, nil, err
 		}
-		fd.Close()
 
-		store, err = deploy.NewBoltDBStore(fd.Name())
+		teardownFn = func() { os.Remove(path) }
+
+		store, err = deploy.NewBoltDBStore(path)
 		if err != nil {
-			return nil, func() { os.Remove(fd.Name()) }, err
+			return nil, teardownFn, err
 		}
 
-		return store, func() { os.Remove(fd.Name()) }, nil
+		return store, teardownFn, nil
 	}})
 }
 
-func TestBoltDBStore_All(t *testing.T) {
+func TestBoltDBStore_AsRepository(t *testing.T) {
+	suite.Run(t, &RepositorySuite{Setup: func() (repo deploy.Repository, setFn func(string, deploy.Deploy), teardownFn func(), err error) {
+		path, err := tempDBFilePath()
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		teardownFn = func() { os.Remove(path) }
+
+		r, err := deploy.NewBoltDBStore(path)
+		if err != nil {
+			return nil, nil, teardownFn, err
+		}
+
+		return r, r.Set, teardownFn, nil
+	}})
+}
+
+func tempDBFilePath() (string, error) {
 	fd, err := ioutil.TempFile(os.TempDir(), "doppelganger")
-	require.NoError(t, err)
+	if err != nil {
+		return "", err
+	}
 	fd.Close()
-	defer os.Remove(fd.Name())
 
-	store, err := deploy.NewBoltDBStore(fd.Name())
-	require.NoError(t, err)
-
-	now := time.Now()
-	key := "key1"
-	user := slack.User{ID: "1", Name: "User 1"}
-
-	var deploys []deploy.Deploy
-	for delta := -10 * time.Minute; delta < 0; delta += time.Minute {
-		d := deploy.New(user, fmt.Sprintf("Deploy from %s ago", delta))
-		d.StartedAt = now.Add(delta)
-		if delta+time.Minute < 0 {
-			d.FinishedAt = now.Add(delta + time.Minute)
-		}
-
-		store.Set("key1", d)
-		deploys = append(deploys, d)
-	}
-
-	allDeploys := store.All(key)
-	if assert.Len(t, allDeploys, len(deploys)) {
-		for i, d := range allDeploys {
-			assert.Equal(t, deploys[i].User, d.User)
-			assert.Equal(t, deploys[i].Subject, d.Subject)
-			assert.True(t, deploys[i].StartedAt.Equal(d.StartedAt), "expected %s, got %s", deploys[i].StartedAt, d.StartedAt)
-			assert.True(t, deploys[i].FinishedAt.Equal(d.FinishedAt), "expected %s, got %s", deploys[i].FinishedAt, d.FinishedAt)
-		}
-	}
+	return fd.Name(), nil
 }
--- deploy/in_memory_store_test.go
+++ deploy/in_memory_store_test.go
@@ -1,48 +1,21 @@
 package deploy_test
 
 import (
-	"fmt"
 	"testing"
-	"time"
 
 	"github.com/andrewslotin/slack-deploy-command/deploy"
-	"github.com/andrewslotin/slack-deploy-command/slack"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
-func TestInMemoryStore(t *testing.T) {
+func TestInMemoryStore_AsStore(t *testing.T) {
 	suite.Run(t, &StoreSuite{Setup: func() (store deploy.Store, teardownFn func(), err error) {
 		return deploy.NewInMemoryStore(), nil, nil
 	}})
 }
 
-func TestBoltDBStore_All(t *testing.T) {
-	store := deploy.NewInMemoryStore()
-
-	now := time.Now()
-	key := "key1"
-	user := slack.User{ID: "1", Name: "User 1"}
-
-	var deploys []deploy.Deploy
-	for delta := -10 * time.Minute; delta < 0; delta += time.Minute {
-		d := deploy.New(user, fmt.Sprintf("Deploy from %s ago", delta))
-		d.StartedAt = now.Add(delta)
-		if delta+time.Minute < 0 {
-			d.FinishedAt = now.Add(delta + time.Minute)
-		}
-
-		store.Set("key1", d)
-		deploys = append(deploys, d)
-	}
-
-	allDeploys := store.All(key)
-	if assert.Len(t, allDeploys, len(deploys)) {
-		for i, d := range allDeploys {
-			assert.Equal(t, deploys[i].User, d.User)
-			assert.Equal(t, deploys[i].Subject, d.Subject)
-			assert.True(t, deploys[i].StartedAt.Equal(d.StartedAt), "expected %s, got %s", deploys[i].StartedAt, d.StartedAt)
-			assert.True(t, deploys[i].FinishedAt.Equal(d.FinishedAt), "expected %s, got %s", deploys[i].FinishedAt, d.FinishedAt)
-		}
-	}
+func TestInMemoryStore_AsRepository(t *testing.T) {
+	suite.Run(t, &RepositorySuite{Setup: func() (repo deploy.Repository, setFn func(string, deploy.Deploy), teardownFn func(), err error) {
+		r := deploy.NewInMemoryStore()
+		return r, r.Set, nil, nil
+	}})
 }
