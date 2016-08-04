64c1346be3a8e1c2be00100d46f1decae3234f88
Andrew Slotin <andrew.slotin@gmail.com>
2016-08-04 18:26:26 +0200

    Remove ServeHTTP from server.Server

--- server/server.go
+++ server/server.go
@@ -1,42 +1,21 @@
 package server
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
-
-	"github.com/andrewslotin/slack-deploy-command/bot"
-	"github.com/andrewslotin/slack-deploy-command/deploy"
-	"github.com/andrewslotin/slack-deploy-command/github"
-	"github.com/andrewslotin/slack-deploy-command/slack"
 )
-
-type DeployEventHandler interface {
-	DeployStarted(channelID string)
-	DeployCompleted(channelID string)
-}
 
 type Server struct {
 	Addr string
 
-	listener   net.Listener
-	slackToken string
-	deploys    *deploy.ChannelDeploys
-	responses  *bot.ResponseBuilder
-
-	deployEventHandlers []DeployEventHandler
+	listener net.Listener
 }
 
-func New(host string, port int, slackToken, githubToken string, store deploy.Store) *Server {
+func New(host string, port int) *Server {
 	return &Server{
-		Addr:       fmt.Sprintf("%s:%d", host, port),
-		slackToken: slackToken,
-		deploys:    deploy.NewChannelDeploys(store),
-		responses:  bot.NewResponseBuilder(github.NewClient(githubToken, nil)),
+		Addr: fmt.Sprintf("%s:%d", host, port),
 	}
 }
 
@@ -47,119 +26,16 @@ func (s *Server) Start(h http.Handler) error {
 	}
 	s.listener = listener
 
-	srv := http.Server{Handler: h}
-	go func() {
-		err := srv.Serve(s.listener)
+	go func(srv *http.Server, ln net.Listener) {
+		err := srv.Serve(ln)
 		if err != nil {
 			log.Fatal(err)
 		}
-	}()
+	}(&http.Server{Handler: h}, s.listener)
 
 	return nil
 }
 
-func (s *Server) AddDeployEventHandler(h DeployEventHandler) {
-	s.deployEventHandlers = append(s.deployEventHandlers, h)
-}
-
 func (s *Server) Shutdown() {
 	s.listener.Close()
-}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Only POST requests are supported", http.StatusBadRequest)
-		return
-	}
-
-	if r.PostFormValue("token") != s.slackToken {
-		http.Error(w, "Invalid token", http.StatusForbidden)
-		return
-	}
-
-	if cmd := r.PostFormValue("command"); cmd != "/deploy" {
-		sendImmediateResponse(w, s.responses.ErrorMessage(cmd, errors.New("not supported")))
-		return
-	}
-
-	channelID := r.PostFormValue("channel_id")
-	user := slack.User{
-		ID:   r.PostFormValue("user_id"),
-		Name: r.PostFormValue("user_name"),
-	}
-
-	switch subject := r.PostFormValue("text"); subject {
-	case "", "help":
-		sendImmediateResponse(w, s.responses.HelpMessage())
-	case "status":
-		d, ok := s.deploys.Current(channelID)
-		if !ok {
-			sendImmediateResponse(w, s.responses.NoRunningDeploysMessage())
-			return
-		}
-
-		sendImmediateResponse(w, s.responses.DeployStatusMessage(d))
-	case "done":
-		d, ok := s.deploys.Finish(channelID)
-		if !ok {
-			sendImmediateResponse(w, s.responses.NoRunningDeploysMessage())
-			return
-		}
-
-		if d.User.ID == user.ID {
-			go sendDelayedResponse(w, r, s.responses.DeployDoneAnnouncement(user))
-		} else {
-			go sendDelayedResponse(w, r, s.responses.DeployInterruptedAnnouncement(d, user))
-		}
-
-		for _, h := range s.deployEventHandlers {
-			go h.DeployCompleted(channelID)
-		}
-	default:
-		d, ok := s.deploys.Start(channelID, deploy.New(user, subject))
-		if !ok {
-			sendImmediateResponse(w, s.responses.DeployInProgressMessage(d))
-			return
-		}
-
-		w.Write(nil)
-
-		go sendDelayedResponse(w, r, s.responses.DeployAnnouncement(user, subject))
-		for _, h := range s.deployEventHandlers {
-			go h.DeployStarted(channelID)
-		}
-	}
-}
-
-func sendImmediateResponse(w http.ResponseWriter, response *slack.Response) {
-	body, err := json.Marshal(response)
-	if err != nil {
-		log.Printf("failed to respond to user with %q (%s)", response.Text, err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(body)
-}
-
-func sendDelayedResponse(w http.ResponseWriter, req *http.Request, response *slack.Response) {
-	responseURL := req.PostFormValue("response_url")
-	if responseURL == "" {
-		log.Printf("cannot send delayed response to a without without response_url")
-		return
-	}
-
-	body, err := json.Marshal(response)
-	if err != nil {
-		log.Printf("failed to respond in channel with %s (%s)", response.Text, err)
-		return
-	}
-
-	slackResponse, err := http.Post(responseURL, "application/json", bytes.NewReader(body))
-	if err != nil {
-		log.Printf("failed to sent in_channel response (%s)", err)
-		return
-	}
-	slackResponse.Body.Close()
 }
