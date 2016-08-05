87e246b011949fe89f30e00e014edfcc290f4f69
Andrew Slotin <andrew.slotin@gmail.com>
2016-08-05 15:23:04 +0200

    Add `/deploy history` command

--- bot/bot.go
+++ bot/bot.go
@@ -86,6 +86,8 @@ func (b *Bot) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		for _, h := range b.deployEventHandlers {
 			go h.DeployCompleted(channelID)
 		}
+	case "history":
+		sendImmediateResponse(w, b.responses.DeployHistoryLink(r.Host, channelID))
 	default:
 		d, ok := b.deploys.Start(channelID, deploy.New(user, subject))
 		if !ok {
