eff798028dae1f9ac6f49d071bc0d9c8c556a8e3
Andrew Slotin <andrew.slotin@gmail.com>
2016-09-02 01:14:14 +0200

    Use custom JSON presenter for deploys

--- dashboard/formatters/json.go
+++ dashboard/formatters/json.go
@@ -3,6 +3,7 @@ package formatters
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/andrewslotin/michael/deploy"
 )
@@ -11,12 +12,27 @@ var (
 	JSON jsonFormatter
 )
 
+type jsonPresenter struct {
+	Author     string    `json:"author"`
+	Subject    string    `json:"subject"`
+	StartedAt  time.Time `json:"started_at"`
+	FinishedAt time.Time `json:"finished_at,omitempty"`
+}
+
 type jsonFormatter struct{}
 
 func (jsonFormatter) RespondWithHistory(w http.ResponseWriter, history []deploy.Deploy) error {
 	w.Header().Set("Content-Type", "application/json")
 
-	data, err := json.Marshal(history)
+	v := make([]jsonPresenter, len(history))
+	for i, d := range history {
+		v[i].Author = d.User.Name
+		v[i].Subject = d.Subject
+		v[i].StartedAt = d.StartedAt
+		v[i].FinishedAt = d.FinishedAt
+	}
+
+	data, err := json.Marshal(v)
 	if err != nil {
 		return err
 	}
