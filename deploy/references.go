5e4502ec098933c93834c507b0a8e58669547146
Andrew Slotin <andrew.slotin@gmail.com>
2017-01-14 17:25:21 -0400

    Unify reference extraction code

--- deploy/references.go
+++ deploy/references.go
@@ -27,41 +27,12 @@ type PullRequestReference struct {
 }
 
 func FindPullRequestReferences(s string) []PullRequestReference {
-	scanner := bufio.NewScanner(strings.NewReader(s))
-	scanner.Split(bufio.ScanWords)
-
 	var refs []PullRequestReference
-	for scanner.Scan() {
-		word := scanner.Text()
-
-		for _, re := range pullRequestReferenceRegexes {
-			if ref, ok := extractPullRequestReference(word, re); ok {
-				refs = append(refs, ref)
-			}
-		}
-	}
+	findReferences(s, pullRequestReferenceRegexes, func(matches map[string]string) {
+		refs = append(refs, PullRequestReference{Repository: matches["repository"], ID: matches["number"]})
+	})
 
 	return refs
-}
-
-func extractPullRequestReference(s string, re *regexp.Regexp) (ref PullRequestReference, ok bool) {
-	m := re.FindStringSubmatch(s)
-	if m == nil {
-		return ref, false
-	}
-
-	for i, name := range re.SubexpNames() {
-		switch name {
-		case "repository":
-			ref.Repository = m[i]
-		case "number":
-			ref.ID = m[i]
-		default:
-			continue
-		}
-	}
-
-	return ref, true
 }
 
 type UserReference struct {
@@ -70,39 +41,33 @@ type UserReference struct {
 }
 
 func FindUserReferences(s string) []UserReference {
-	scanner := bufio.NewScanner(strings.NewReader(s))
-	scanner.Split(bufio.ScanWords)
-
 	var refs []UserReference
-	for scanner.Scan() {
-		word := scanner.Text()
-
-		for _, re := range userReferenceRegexes {
-			if ref, ok := extractUserReference(word, re); ok {
-				refs = append(refs, ref)
-			}
-		}
-	}
+	findReferences(s, userReferenceRegexes, func(matches map[string]string) {
+		refs = append(refs, UserReference{ID: matches["userid"], Name: matches["username"]})
+	})
 
 	return refs
 }
 
-func extractUserReference(s string, re *regexp.Regexp) (ref UserReference, ok bool) {
-	m := re.FindStringSubmatch(s)
-	if m == nil {
-		return ref, false
-	}
+func findReferences(s string, regexes []*regexp.Regexp, f func(map[string]string)) {
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	scanner.Split(bufio.ScanWords)
 
-	for i, name := range re.SubexpNames() {
-		switch name {
-		case "username":
-			ref.Name = m[i]
-		case "userid":
-			ref.ID = m[i]
-		default:
-			continue
+	for scanner.Scan() {
+		word := scanner.Text()
+
+		for _, re := range regexes {
+			m := re.FindStringSubmatch(word)
+			if m == nil {
+				continue
+			}
+
+			matches := make(map[string]string, len(re.SubexpNames()))
+			for i, name := range re.SubexpNames() {
+				matches[name] = m[i]
+			}
+
+			f(matches)
 		}
 	}
-
-	return ref, true
 }
